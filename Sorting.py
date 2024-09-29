7027512bbfb2f7fe2505f18b13bdf493bbf39165
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2024-09-29 11:27:53 +0530

    Sorting Algo Completed :)

--- Sorting.py
+++ Sorting.py
@@ -12,7 +12,7 @@ class arr:
     def selection_sort(self):
         for i in range(len(self.array)):
             smallest = i
-            for j in range(len(self.array)-1):
+            for j in range(i+1,len(self.array)):
                 if self.array[smallest] > self.array[j]:
                     smallest=j 
         self.array[i],self.array[smallest] =self.array[smallest],self.array[i]
@@ -41,19 +41,53 @@ class arr:
         return j+1 
             
     
+    
     def quick_sort(self,low,high):
         if low < high:
             pivot = self.partition(low,high)
             self.quick_sort(low,pivot-1)
             self.quick_sort(pivot+1,high)
         return self.array
+    
+    def conquer(self,start,end,mid):
+        index1= start
+        index2= mid+1
+        merge =[0]*end-start+1
+        ind=0
+        while index1 <=mid and index2<=end:
+            if self.array[index1] < self.array[index2]:
+                merge[ind] = self.array[index1]
+                index1+=1
+            else:
+                merge[ind] = self.array[index2]
+                index2+=1
+            ind+=1
+        while index1 <= mid:
+            merge[ind] = self.array[index1]
+            ind+=1
+            index1+=1
+        while index2 <= end:
+            merge[ind] = self.array[index2] 
+            ind+=1
+            index2+=1
+        for i in range(len(merge)):
+            self.array[start+i] = merge[i]
+                        
+                
         
-            
+        
+    def merge_sort(self,start,end):
+        if start < end:
+            mid = (start +(end-start))//2
+            self.merge_sort(start,mid)   
+            self.merge_sort(mid+1,end)
+            self.conquer(start,end,mid)                 
         
 if __name__ == "__main__":
     array = arr([1,343,0,112])
     #print(array.bubble_sort())
     #print(array.selection_sort())
     #print(array.insertion_sort())
-    print(array.quick_sort(0,3))
+    # print(array.quick_sort(0,3))
+    print(array.merge_sort(0,3))
                 
