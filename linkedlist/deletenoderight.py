c6faa8d38f2d100800153ef3125be6ce9d221944
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2025-01-20 10:58:42 +0530

    Delete nodes which have a greater value on right side

---
+++ linkedlist/deletenoderight.py
@@ -0,0 +1,60 @@
+# Delete nodes which have a greater value on right side
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+        
+def insert_end(head,data):
+    
+    node = Node(data)
+    if not head:
+        return node
+    curr =head
+    while curr.next:
+        curr =curr.next 
+    curr.next = node
+    return head
+
+def deletenode(head):
+    
+    #reverseing 
+    prev,curr = None,head
+    while curr:
+        nextnode = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nextnode
+        
+    temp = prev
+    maxnode = prev    
+    
+    while temp and temp.next:
+        if maxnode.data > temp.next.data:
+            temp.next = temp.next.next
+        else:
+            temp = temp.next
+            maxnode = temp
+            
+    prev,curr = None,prev
+    while curr:
+        nextnode = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nextnode
+    return prev
+
+
+def printing(head):
+    while head:
+        print(head.data,end=" ")
+        head = head.next
+            
+if __name__ == "__main__":
+    # 12->15->10->11->5->6->2->3
+    list1 = [12,15,10,11,5,6,2,3]
+    head = None
+    for i in list1:
+        head = insert_end(head,i)
+        
+    printing(deletenode(head))  
